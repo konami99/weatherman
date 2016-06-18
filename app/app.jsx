0b6a0d73843dc9d60459d8a8d1bc5480cd00f4e2
Richard Chou <konami99@hotmail.com>
2016-06-18 22:04:19 +1000

    weatherman

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,10 @@ var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 
+// load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
