1d4b267fcb7ee71f014eb887a97504b25b119d60
Richard Chou <konami99@hotmail.com>
2016-07-01 16:08:45 +1000

    migration

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,15 @@
 var React = require('react');
 
 
-var WeatherMessage = React.createClass({
-    render: function(){
-        var location = this.props.location;
-        var temp = this.props.temp;
-        return (
-            <h2>it is {temp} in {location}</h2>
-        );
-    }
-})
+
+
+var WeatherMessage = (props) => {
+    var location = props.location;
+    var temp = props.temp;
+    return (
+        <h2>it is {temp} in {location} !?</h2>
+    )
+};
+
+
 module.exports = WeatherMessage;
