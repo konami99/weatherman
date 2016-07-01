f789286ac23b422619dd0d6bdca858e9f002e9e8
Richard Chou <konami99@hotmail.com>
2016-07-01 15:58:26 +1000

    weatherman

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,9 @@
 var React = require('react');
 
+var About = (props) => {
+    return (
+        <h2>About Component !!??</h2>
+    )
+};
 
-var About = React.createClass({
-    render: function(){
-        return (
-            <h2>About Component</h2>
-        );
-    }
-})
 module.exports = About;
